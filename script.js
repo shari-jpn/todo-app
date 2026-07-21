3edc5578af774cf7b5ae52f834287ac891577539
sharik parejo <sparejo2@cuc.edu.co>
2026-07-21 18:18:42 -0500

    Agregar tareas y eliminar tareas

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ const taskList = document.getElementById("taskList");
 
 addButton.addEventListener('click', () => {
     const taskText = taskInput.value;
+    if (taskText == ''){
+        alert('Por favor, escribe una tarea :)');
+        return;
+    }
     const newTask = document.createElement('li');
+    const deleteButton = document.createElement('button');
     newTask.textContent = taskText;
+    deleteButton.textContent = '❌'
+    deleteButton.addEventListener('click',() => {
+        newTask.remove();
+    })
+    newTask.appendChild(deleteButton);
     taskList.appendChild(newTask);
     taskInput.value ='';
 });
