83f81dee2164a6511d7e24e037febb27d92bbd48
sharik parejo <sparejo2@cuc.edu.co>
2026-07-21 21:33:23 -0500

    Agragar tareas con localStorage

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # My To-Do list
-## Day 1
-- I thought about what project I could do for my first project
-- Create the project structure
-- 
+My first to-do list application developed with HTML, CSS, and JavaScript.
+
+## Description
+This application allows you to create, complete, and delete tasks. Additionally, tasks are automatically saved in the browser using localStorage, so they are not lost when closing the page.
+
+## What does the page?
+- Create tasks
+- Delete tasks
+- Mark tasks as completed
+- Add tasks by pressing the Enter key.
+- Validate empty tasks
+- Save tasks using localStore
+
+## what I learned
+Well, with this project I learned to:
+- Manipulate the DOM with JavaScript
+- Create and use functions
+- Handle events
+- Save data using funtions
+- Better organize my code
+
+## Future improvements
+- Edit tasks
+- Add categories
+- Improve the desing
+- Create a dark mode
--- script.js
+++ script.js
@@ -2,22 +2,82 @@ const taskInput = document.getElementById("taskInput");
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 
-addButton.addEventListener('click', () => {
-    const taskText = taskInput.value;
-    if (taskText == ''){
-        alert('Por favor, escribe una tarea :)');
-        return;
-    }
+// se encarga de crear la tarea y mostrarla
+function createTask(taskText) {
     const newTask = document.createElement('li');
     const deleteButton = document.createElement('button');
-    newTask.textContent = taskText;
-    deleteButton.textContent = '❌'
-    deleteButton.addEventListener('click',() => {
+    const taskSpan = document.createElement('span');
+
+    taskSpan.textContent = taskText;
+
+    taskSpan.addEventListener('click', () => {
+        taskSpan.classList.toggle('completed');
+        saveTasks();
+    });
+
+    newTask.appendChild(taskSpan);
+
+    deleteButton.textContent = '❌';
+
+    deleteButton.addEventListener('click', () => {
         newTask.remove();
-    })
+        saveTasks();
+    });
+
     newTask.appendChild(deleteButton);
+
     taskList.appendChild(newTask);
-    taskInput.value ='';
+
+    saveTasks();
+
+    taskInput.value = '';
+}
+
+// funcion para guardar tareas
+function saveTasks() {
+    const tasks = [];
+
+    document.querySelectorAll('li').forEach((task) => {
+        tasks.push({
+            text: task.querySelector('span').textContent,
+            completed: task
+                .querySelector('span')
+                .classList.contains('completed'),
+        });
+    });
+    console.log(tasks);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// obtiene el texto del cuadro y verifica que no este vacio
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === '') {
+        alert('por favor, escribe una tarea :)');
+        return;
+
+    }
+    createTask(taskText);
+}
+
+addButton.addEventListener('click', addTask);
+
+taskInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addTask();
+    }
 });
 
+const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
+savedTasks.forEach((task) => {
+    createTask(task.text);
+
+    if (task.completed){
+        const lastTask =
+            taskList.lastElementChild.querySelector('span');
+
+        lastTask.classList.add('completed');
+    }
+})
